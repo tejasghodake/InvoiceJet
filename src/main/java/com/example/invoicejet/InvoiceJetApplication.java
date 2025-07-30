6d454a2f2026196b1c70f8cb4d57ca5a2259561d
tejasghodake <tejasghodake24@gmail.com>
2025-07-30 09:37:24 +0530

    Created InvoiceJetApplication.java

---
+++ src/main/java/com/example/invoicejet/InvoiceJetApplication.java
@@ -0,0 +1,12 @@
+package com.example.invoicejet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceJetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoiceJetApplication.class, args);
+    }
+}
